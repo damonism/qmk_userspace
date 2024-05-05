e74e2a503376b3359b39aadf99d5d82cf8923dfb
Damon Muller <admin@ausvotes.org>
2024-05-05 11:46:59 +1000

    Carried over

---
+++ keyboards/boardsource/lulu/rp2040/keymaps/damonism/config.h
@@ -0,0 +1,9 @@
+// Copyright 2022 Cole Smith <cole@boadsource.xyz>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define RGB_MATRIX_LED_COUNT 70
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 500
+
